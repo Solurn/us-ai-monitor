23337fd0c54202c5d46a01d811a167d4aab6d70e
Solurn <Solurn@users.noreply.github.com>
2026-04-30 20:08:09 +0800

    Update Taiwan self report 2026-04-30

--- web/data/self-report-history.js
+++ web/data/self-report-history.js
@@ -1,6 +1,66 @@
 window.selfReportHistory = {
   "items": [
     {
+      "generatedAt": "2026-04-30T20:08:08+08:00",
+      "queryDate": "2026-04-30",
+      "displayDate": "115年4月30日",
+      "count": 2,
+      "skipped": {
+        "bond_subject": 0,
+        "missing_financials": 2,
+        "duplicate": 0,
+        "detail_error": 0
+      },
+      "rows": [
+        {
+          "code": "2464",
+          "name": "盟立",
+          "market": "上市",
+          "announcement_date": "115/04/30",
+          "subject": "係因本公司有價證券於集中交易市場達公布注意交易資訊 標準，故公布相關財務業務等重大訊息，以利投資人區別瞭解",
+          "month_label": "3月",
+          "monthly_revenue": 652.0,
+          "monthly_pre_tax_income": 30.0,
+          "monthly_parent_income": null,
+          "monthly_eps": 0.13,
+          "quarter_eps": -0.26,
+          "tax_margin_pct": 4.601226993865031,
+          "operating_margin_pct": null,
+          "previous_quarter_monthly_eps": -0.08666666666666667,
+          "reference_quarter": "2025Q4",
+          "reference_tax_margin_pct": -3.21,
+          "reference_operating_margin_pct": -1.13,
+          "tax_margin_delta_pct": 7.811226993865031,
+          "operating_margin_delta_pct": null,
+          "detail_key": "sii:2464:20260430:155649:1"
+        },
+        {
+          "code": "4529",
+          "name": "淳紳",
+          "market": "上櫃",
+          "announcement_date": "115/04/30",
+          "subject": "係因本公司有價證券於集中交易市場達公布注意交易資訊標 準，故公布相關財務業務等重大訊息，以利投資人區別瞭解。",
+          "month_label": "3月",
+          "monthly_revenue": 0.67,
+          "monthly_pre_tax_income": -4.0,
+          "monthly_parent_income": -4.0,
+          "monthly_eps": -0.047,
+          "quarter_eps": -0.162,
+          "tax_margin_pct": -597.0149253731342,
+          "operating_margin_pct": -597.0149253731342,
+          "previous_quarter_monthly_eps": -0.054,
+          "reference_quarter": "2025Q4",
+          "reference_tax_margin_pct": -1177.0,
+          "reference_operating_margin_pct": -1148.0,
+          "tax_margin_delta_pct": 579.9850746268658,
+          "operating_margin_delta_pct": 550.9850746268658,
+          "detail_key": "otc:4529:20260430:143613:1"
+        }
+      ],
+      "image": "./assets/self-report/self-report-2026-04-30.png",
+      "source": "公開資訊觀測站"
+    },
+    {
       "generatedAt": "2026-04-29T20:41:31+08:00",
       "queryDate": "2026-04-29",
       "displayDate": "115年4月29日",
