9aa220c08f424a03eb927456bf3861f7bb8b3833
Solurn <Solurn@users.noreply.github.com>
2026-06-01 20:03:31 +0800

    Update Taiwan monthly revenue 115/5

---
+++ web/data/tw-revenue-latest.js
@@ -0,0 +1,30 @@
+window.twRevenueLatest = {
+  "generatedAt": "2026-06-01T20:03:31+08:00",
+  "period": "115/5",
+  "source": "MOPS 月營收彙總表",
+  "filters": {
+    "markets": [
+      "sii",
+      "otc"
+    ],
+    "momThresholdPct": 30.0,
+    "yoyThresholdPct": 30.0,
+    "requirePositiveYtdYoy": true,
+    "requireNonNegativeMomYoy": true,
+    "excludedIndustryKeywords": [
+      "生技",
+      "建材營造",
+      "營建",
+      "金融"
+    ],
+    "publicIssueDateCutoff": "89/01/01"
+  },
+  "stats": {
+    "selected": 0,
+    "stories": 0,
+    "highs": 0,
+    "storyHighs": 0
+  },
+  "selected": [],
+  "stories": []
+};
