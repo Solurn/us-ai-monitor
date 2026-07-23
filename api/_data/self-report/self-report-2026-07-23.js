04cc6415f0d1c5b63aa4a4d9899f40f2bc78cae2
Solurn <Solurn@users.noreply.github.com>
2026-07-23 20:38:10 +0800

    Update Taiwan self report 2026-07-23

--- api/_data/self-report/self-report-2026-07-23.js
+++ api/_data/self-report/self-report-2026-07-23.js
@@ -1,4 +1,5 @@
-window.selfReportLatest = {
+window.selfReportArchiveItems = window.selfReportArchiveItems || {};
+window.selfReportArchiveItems["2026-07-23"] = {
   "generatedAt": "2026-07-23T20:37:26+08:00",
   "queryDate": "2026-07-23",
   "displayDate": "115年7月23日",
