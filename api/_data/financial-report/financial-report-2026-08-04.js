723743e73b675509a17cbbf2197b6ac2889b3cfd
Solurn <Solurn@users.noreply.github.com>
2026-08-04 21:53:30 +0800

    Update Taiwan financial reports 2026-08-04

--- api/_data/financial-report/financial-report-2026-08-04.js
+++ api/_data/financial-report/financial-report-2026-08-04.js
@@ -1,4 +1,5 @@
-window.financialReportLatest = {
+window.financialReportArchiveItems = window.financialReportArchiveItems || {};
+window.financialReportArchiveItems["2026-08-04"] = {
   "generatedAt": "2026-08-04T21:49:26+08:00",
   "queryDate": "2026-08-04",
   "rocYear": 115,
